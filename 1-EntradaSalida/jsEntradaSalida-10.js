71d5c642c4a8c9c6aa3ad6154e039cb298b88342
Agustin-Defranchi <agustin.i.defranchi@gmail.com>
2021-02-02 20:33:05 -0300

    finalizado

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,7 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var sueldo = txtIdImporte.value;
+	sueldo = parseInt(sueldo);
+	txtIdResultado.value = sueldo * 0.75; 
 }
