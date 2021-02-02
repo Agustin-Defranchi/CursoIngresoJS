96440da4d2542347278d7b81280b6d4d498cdc3e
Agustin-Defranchi <agustin.i.defranchi@gmail.com>
2021-02-01 22:59:42 -0300

    nuevo cambio

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,7 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var dato = prompt("Rellene el recuadro.","ingrese el texto aqui");
+	alert("" + dato);
 }
-
+	
