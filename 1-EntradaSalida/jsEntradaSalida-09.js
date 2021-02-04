cc959006e3bb7d6a937617855b6af8806d1ecf6f
Agustin-Defranchi <agustin.i.defranchi@gmail.com>
2021-02-04 12:33:31 -0300

    finalizado

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -6,8 +6,16 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 	let sueldo = document.getElementById("txtIdSueldo").value;
-
-
-	sueldo = parseInt(sueldo);
+	sueldo = parseFloat(sueldo);
 	document.getElementById("txtIdResultado").value = sueldo * 1.10; 
+	
+/*	let sueldo = document.getElementById("txtIdSueldo").value;
+	sueldo = parseFloat(sueldo);
+	let resultado = sueldo + (sueldo * 10 / 100);
+	
+	document.getElementById("txtIdResultado").value = resultado;*/
 }
+//obtengo el valor del sueldo
+//transformo el sueldo a entero
+//obtengo el sueldo aumentado
+//muestro el sueldo aumentado el el cuadro de texto
